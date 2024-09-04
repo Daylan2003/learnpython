abd4a5fb1e555757850bf99ce644aab786ce25c5
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-04 17:27:30 -0400

    Lesson 2 Completed

---
+++ variables.py
@@ -0,0 +1,28 @@
+thisint = 7
+print (thisint)
+
+thisfloat = 7.0
+print (thisfloat)
+
+thisfloat = float(7)
+print (thisfloat)
+
+print ("Now lets look at strings")
+
+thisstring = 'hello'
+print (thisstring)
+
+thisstring = "hello"
+print (thisstring)
+
+print ("Double quotes make it easy to use apostrophes")
+
+hello = "hello"
+world = ' world'
+helloworld = hello + world
+print (helloworld)
+
+a, b = 3, 4
+print (a, b)
+
+print("You cannot mix numbers, operators and strings")
