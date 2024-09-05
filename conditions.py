af53a254b4aec1b3ca9a6fd6cf7adca8740e219e
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-05 00:21:31 -0400

    Lesson 7, operators Completed

---
+++ conditions.py
@@ -0,0 +1,14 @@
+x = 2
+print(x == 2)
+print(x == 3)
+print(x == 4)
+
+name = "John"
+
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick")
+
+x = [1,2,3]
+y = [1,2,3]
+print(x == y)
+print(x is y)    
---
+++ exercise5.py
@@ -0,0 +1,22 @@
+number = 10
+second_number = 10
+first_array = [1]
+second_array = [1,2,3]
+
+if number > 5:
+    print("1")
+
+if not first_array[0] == 3:
+    print("2")
+
+if len(second_array) == 3:
+    print("3")
+
+if len(first_array) + len(second_array) == 4:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number == 4:
+    print("6")                    
