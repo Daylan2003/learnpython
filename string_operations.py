d793fd694cc52c2ae75294fae7fa9fcef09f561c
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-04 23:53:56 -0400

    Lesson 6, string operations Completed

---
+++ string_operations.py
@@ -0,0 +1,24 @@
+astring = "hello"
+
+print("single quotes are ''")
+
+print(len(astring))
+print(astring.index("l"))
+print(astring.count("l"))
+print(astring[2:4])
+
+astring = "Hello World"
+print(astring[::-1])
+afewwords = astring.split(" ")
+print(afewwords)
+
+newstring = "  Hello World"
+print(len(newstring))
+print(newstring[-1])
+print(newstring[0:3])
+
+print("The escape character in Python is the backslash")
+print("for example\"")
+
+print(newstring.title())
+print(newstring.strip())
