397c8aeb6a09f78a11b8cc44ef1fa452c0dd5e60
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-04 20:58:59 -0400

    Lesson 4, operators Completed

---
+++ exercise3.py
@@ -0,0 +1,5 @@
+x_list = ["x" * 10]
+y_list = ["y" * 10]
+big_list = x_list + y_list
+
+print(big_list)
---
+++ operators.py
@@ -0,0 +1,26 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+print("Two multiplication symbols makes a power relationship")
+
+squared = 7 ** 2
+cubed = 7 ** 3
+
+print(squared)
+print(cubed)
+
+print("We can also multiple strings")
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
