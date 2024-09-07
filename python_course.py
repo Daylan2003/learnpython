62b9b0517ab20d89068bdb4e18df0808b32a7ba4
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-06 23:04:43 -0400

    Python 06/09 11:04

---
+++ python_course.py
@@ -0,0 +1,46 @@
+name = "Daylan"
+print(name)
+print(type(name))
+
+age = 20
+print(age)
+print(type(age))
+
+print("My name is %s and I am %d years old" % (name, age))
+
+height = 250.5
+print(height)
+print(type(height))
+print("Your height is " + str(height) + "cm")
+
+human = True
+print("Are you a human: " + str(human)) 
+print(type(human))
+
+#string methods
+
+name = "daylan"
+print(len(name))
+print(name.find("y"))
+print(name.capitalize())
+print(name.upper())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("a"))
+print(name.replace("a","o"))
+print(name * 3)
+
+x = 1
+y = 2.0
+z = "3"
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z*3)
+
+
+
