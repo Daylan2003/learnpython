30cc7b38471ed46da6c5caf6d2ddc2754049fbea
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-08 01:00:57 -0400

    Python 08/09 1:00

---
+++ arguements.py
@@ -0,0 +1,19 @@
+"""def add(*stuff):
+    sum = 0
+    stuff = list(stuff)
+    stuff[0] = 0
+    for i in stuff:
+        sum += i
+    return sum
+
+print(add(1,2,3))"""
+
+#kwargs = parameter that will pack all arguements into a dictionary
+
+def hello(**kwargs):
+    #print("Hello " + kwargs['first'] + " " + kwargs['last'])
+    print("Hello", end=" ")
+    for x,y in kwargs.items():
+        print(y, end=" ")
+
+hello(first="Daylan", middle="Pravir", last="Maharaj")    
--- string_formatting.py
+++ string_formatting.py
@@ -1,4 +1,4 @@
-name = "John"
+"""name = "John"
 print("Hello %s!" % name)
 
 age = 23
@@ -6,4 +6,21 @@ age = 23
 print("%s is %d years old." % (name, age))
 
 mylist = [1,2,3]
-print("A list: %s" % mylist)
+print("A list: %s" % mylist)"""
+
+#animal = "cow"
+#item = "moon"
+
+#print("The %s jumped over the %s" %(animal, item))
+#print("The {} jumped over the {}".format(animal, item))
+
+"""name = "Daylan"
+
+print("Hello, my name is {}".format(name))
+print("Hello, my name is {:>10}. Nice to meet you".format(name))"""
+
+number = -10000000000
+
+#print("The number pi is {:e}".format(number))
+
+print
